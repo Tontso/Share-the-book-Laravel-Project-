0ceeba2001bcc3773a462e36dcb59f320067028a
Tontso <tommy.tonchev@outlook.com>
2022-02-23 17:23:51 +0200

    Add Login/Register Service

--- library-project/routes/web.php
+++ library-project/routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+Route::get('/post', function() {
+    return view('post');
+});
+
+require __DIR__.'/auth.php';
